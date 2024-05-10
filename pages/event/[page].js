0ff0aede523b90583514708379444a75f661e1b2
Krzysztof Brylski <krzysztof.brylski@codewave.pl>
2024-05-10 09:28:20 +0200

    #24629 fixed code formating

--- pages/event/[page].js
+++ pages/event/[page].js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {Helmet} from 'react-helmet'
+import { Helmet } from 'react-helmet'
 import Layout from '../../layouts/layout'
 import EventDescriptionCard from '../../components/event/EventDescriptionCard'
 import Contact from '../../sections/Contact'
@@ -10,15 +10,15 @@ import {
     getEventPrevious,
     getEventNext,
 } from '../../lib/event'
-import {replaceUndefinedWithNull} from '../../lib/sanitize';
+import { replaceUndefinedWithNull } from '../../lib/sanitize'
 import config from '../../lib/config'
 import FlotiqImage from '../../lib/flotiqImage'
 
-const EventTemplate = ({data, pageContext}) => (
+const EventTemplate = ({ data, pageContext }) => (
     <Layout additionalClass={['bg-white']}>
         <Helmet>
             <title>{config.name}</title>
-            <meta name="description" content={config.description}/>
+            <meta name="description" content={config.description} />
         </Helmet>
         <div className="flex flex-wrap max-w-7xl mx-auto px-2 sm:px-6 lg:px-0">
             <EventDescriptionCard
@@ -49,12 +49,18 @@ const EventTemplate = ({data, pageContext}) => (
 
 export default EventTemplate
 
-export async function getStaticProps({params}) {
+export async function getStaticProps({ params }) {
     const event = await getEventBySlug(params.page)
     const filtersMoreEvents = `{"date":{"type":"greaterThan","filter":"${event.data[0].date}"}}`
-    const moreEvents = replaceUndefinedWithNull(await getEvents(1, 3, filtersMoreEvents))
-    const nextEvents = replaceUndefinedWithNull(await getEventNext(event.data[0].date))
-    const prevEvents = replaceUndefinedWithNull(await getEventPrevious(event.data[0].date))
+    const moreEvents = replaceUndefinedWithNull(
+        await getEvents(1, 3, filtersMoreEvents)
+    )
+    const nextEvents = replaceUndefinedWithNull(
+        await getEventNext(event.data[0].date)
+    )
+    const prevEvents = replaceUndefinedWithNull(
+        await getEventPrevious(event.data[0].date)
+    )
 
     return {
         props: {
@@ -72,10 +78,10 @@ export async function getStaticPaths() {
     const fetchAllEvents = await getEvents(1, 10)
     const allEvents = fetchAllEvents.data
     const paths = []
-    const eventsCount = allEvents ? allEvents.length : 0;
+    const eventsCount = allEvents ? allEvents.length : 0
     for (let i = 0; i < eventsCount; i += 1) {
         paths.push({
-            params: {page: allEvents[i].slug},
+            params: { page: allEvents[i].slug },
         })
     }
 
